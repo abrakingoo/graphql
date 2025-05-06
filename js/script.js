b26c4deb8a9ed6530d357736492bf66622521ae3
abrakingoo <maikoabraham68@gmail.com>
2025-05-06 14:09:47 +0300

    feat: get the user name and password from the login page

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const form = document.getElementsByTagName("form")[0];
+form.addEventListener("submit", () => {
+    const userName = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+})
